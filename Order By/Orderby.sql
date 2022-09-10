a8c9c5b557c2cfdb35a37c7f872ef784ff9dfa05
Hugox96 <alveshugo2906@gmail.com>
2022-09-09 23:39:14 -0300

    estudando order by

---
+++ Order By/Orderby.sql
@@ -0,0 +1,6 @@
+-- ORDER BY is used with SELECT to sort the returned data.
+
+-- The following example sorts our customers table by the FirstName column.
+
+SELECT * From customers
+ORDER BY FirstName;
