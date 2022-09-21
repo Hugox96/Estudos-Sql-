a73886270a31a8b0b57127121784cff445c791ad
Hugox96 <alveshugo2906@gmail.com>
2022-09-21 11:25:57 -0300

    estudando order by

--- Order By/Orderby.sql
+++ Order By/Orderby.sql
@@ -4,6 +4,9 @@
 SELECT * From customers
 ORDER BY FirstName; 
 
+-- The ORDER BY keyword sorts the records in ascending order by default. 
+-- To sort the records in descending order, use the DESC keyword.
+
 -- Select ''id'' from ''students''. Order the results by id, in descending order.
 SELECT id from students
 ORDER By id Desc
