b9e4c298552a0b77e98e182f21fb50efadff6bf8
Hugox96 <alveshugo2906@gmail.com>
2023-11-17 09:41:54 -0300

    ok

---
+++ Exercicios/Desafio_Issac.sql
@@ -0,0 +1,20 @@
+-- Crie uma consulta que nos traga o valor total da receita do primeiro semestre do ano
+-- de 2023, considerando as diferentes alíquotas de take rate cobradas pelo Isaac, e deixe
+-- registrado o código SQL usado (pode usar print ou qualquer outra forma que for
+-- necessária para mostrar o código). 
+
+
+
+id| mês | mensalidade | escola | take_rate |enrollment_take _rate
+
+1 | 2023-01-01 | 700,00 |  Objetivo 9% 6%
+2 |  2023-02-01| 1.200,00| Objetivo 9% 6%
+3 | 2023-03-01 | 350,00 |  Fisk 16% 13%
+4 | 2023-04-01 | 2.300,00 | Objetivo 9% 6%
+5 |  2023-05-01| 820,00 |   Fisk 16% 13%
+6 | 2023-06-01 | 500,00 |   Fisk 16% 13%
+
+
+select sum(mensalidade * take rate)
+from state_revenue
+group by mensalidade
