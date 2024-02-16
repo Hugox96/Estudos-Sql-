9618ef483d6c5e0aa74fd09396edbe3c3d565398
Hugox96 <alveshugo2906@gmail.com>
2024-02-16 16:22:54 -0300

    ok

--- Table_Of_Constrain/Primary_Key.sql
+++ Table_Of_Constrain/Primary_Key.sql
@@ -14,3 +14,17 @@ Create Table Persons(
     primary key (id) 
 )
 
+-- SQL Server / Oracle / MS Access:
+
+CREATE TABLE Persons (
+    ID int NOT NULL PRIMARY KEY,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int
+);
+
+-- To create a PRIMARY KEY constraint on the "ID" column when the table is already created, use the following SQL:
+Alter Table Persons(
+    Add primary key(id)
+)
+
