2645324ef51a16f6f2c1f38bad1754747098bb7a
Hugox96 <alveshugo2906@gmail.com>
2024-02-16 16:19:10 -0300

    ok

---
+++ Table_Of_Constrain/Primary_Key.sql
@@ -0,0 +1,16 @@
+-- The primary key constraint is used to uniquely identify rows of a table.
+-- In most cases, the primary key is the auto_increment column.
+-- So, for our Customers and PhoneNumbers tables, it's the id column.
+
+-- It is set when creating the table:
+
+-- MySQL:
+
+Create Table Persons(
+    Id int not null,
+    lastname varchar(255) not null
+    firstname varchar(255) 
+    age id
+    primary key (id) 
+)
+
