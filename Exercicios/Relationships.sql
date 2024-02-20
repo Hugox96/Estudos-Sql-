4b1dc5c88fd9f374f84e0c50f007bb86a10e2fad
Hugox96 <alveshugo2906@gmail.com>
2024-02-20 11:17:37 -0300

    ok

--- Exercicios/Relationships.sql
+++ Exercicios/Relationships.sql
@@ -2,21 +2,22 @@ SELECT *
 FROM Orders
 JOIN Customers ON Orders.CustomerID = Customers.CustomerID;
 
-Aqui temos uma one-to-many, onde um cliente pode ter varias orders
+-- Aqui temos uma one-to-many, onde um cliente pode ter varias orders
 
 SELECT *
 FROM Products
 LEFT JOIN Categories ON Products.CategoryID = Categories.CategoryID;
 
-One to one, um produto tem uma categoria, o left join indica que mesmo que não um produto não tenha categoria ele ainda será adicionado a tabela
+-- One to one, um produto tem uma categoria, o left join indica que mesmo que não um produto não tenha categoria ele ainda será adicionado a tabela
 
 SELECT *
 FROM Departments
 JOIN Employees ON Departments.DepartmentID; = Employees.DepartmentID;
-One to one, onde um empregado tem um departamento
+
+-- One to one, onde um empregado tem um departamento
 
 SELECT *
 FROM Students
 JOIN Courses ON Students.CourseID = Courses.CourseID;
 
-Many to many, onde um curso pode ter varios estudantes e vice-versa, neste caso ele quer associar o id do students ao id do curso.
+-- Many to many, onde um curso pode ter varios estudantes e vice-versa, neste caso ele quer associar o id do students ao id do curso.
